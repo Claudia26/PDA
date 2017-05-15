4e4cc2e52d8552e0222cbc94237372d5c50ce491
Claudia-PC\Claudia <claudia.mvasile@gmail.com>
2017-05-15 22:16:37 +0300

    15.05

--- MPI1/MPI1/MPI1/Program.cs
+++ MPI1/MPI1/MPI1/Program.cs
@@ -6,43 +6,13 @@ namespace MPI1
 {
     class Program
     {
-
-        public static int procid, numproc, start, finish, j;
+        public static int i, j;
+        public static int start, finish;
+        public static int processId, nrPerProcessor;
 
         const int n = 10;
-        static void Main(string[] args)
-        {
 
-            using (new MPI.Environment(ref args))
-            {
-
-                procid = Communicator.world.Rank;
-                numproc = Communicator.world.Size;
-                Communicator.world.Barrier();
-
-                start = 2 + procid * (n - 1) / numproc;
-                finish = 1 + (procid + 1) * (n - 1) / numproc;
-
-                //check every number in the range of this processor
-                for (j = start; j <= finish; j++)
-                {
-                    //if the number is not composite, is prime, increment the local counter
-                    if (checkPrime(j))
-                    {
-                        Console.WriteLine("prime: " + j);
-                    }
-                }
-
-
-                Console.WriteLine("Process " + procid + " finished\n");
-                //Communicator.world.Reduce<int>(start, Operation<int>.Add, 0);
-            }
-
-        }
-
-
-
-        public static bool checkPrime(int number)
+        public static bool isPrime(int number)
         {
 
             if (number == 1) return false;
@@ -62,5 +32,53 @@ namespace MPI1
 
 
 
+
+        static void Main(string[] args)
+        {
+            int primeNr = 0;
+            int totalNr = 0;
+
+            //initialize MPI Environment
+            using (new MPI.Environment(ref args))
+            {
+                // each communicator has an unique idenification id in order to specify the source and the destination
+                processId = Communicator.world.Rank;
+
+                // size of communicator
+                nrPerProcessor = Communicator.world.Size;
+
+                //Blocks the current process until all other processes in the current communicator have reached this routine.
+                Communicator.world.Barrier();
+
+                start = 2 + processId * (n - 1) / nrPerProcessor;
+                finish = 1 + (processId + 1) * (n - 1) / nrPerProcessor;
+
+                Intracommunicator world = Communicator.world;
+
+
+                for (j = start; j <= finish; j++)
+                {
+                    if (isPrime(j))
+                    {
+                        ++primeNr;
+                        Console.WriteLine("The prime nr {0} has been found", j);
+                    }
+                }
+
+                if (world.Rank == 0)
+                {
+                    //world.Reduce - Combines the values sent by all processes using a predefined operator and places result in the receive buffer of the root process
+                    totalNr = world.Reduce<int>(primeNr, Operation<int>.Add, 0);
+
+                    System.Console.WriteLine("The Number of Prime Numbers found is: {0}", totalNr);
+                }
+                else
+                {
+                    world.Reduce<int>(primeNr, Operation<int>.Add, 0);
+                }
+            }
+
+
+        }
     }
 }
